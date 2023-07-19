b784470c0315e153cbb6fcc051fbb5d30cfb3f2a
A.Kitova <nazumy@bk.ru>
2023-07-19 21:45:33 +0600

    [+] Responses validation methods

--- src/test/java/autotests/clients/DuckActionClient.java
+++ src/test/java/autotests/clients/DuckActionClient.java
@@ -3,8 +3,14 @@ package autotests.clients;
 import autotests.EndpointConfig;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.message.MessageType;
+import com.consol.citrus.message.builder.ObjectMappingPayloadBuilder;
 import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
@@ -48,4 +54,57 @@ public class DuckActionClient extends TestNGCitrusSpringSupport {
                 .queryParam("id", id)
         );
     }
+
+    //region properties - валидация
+
+    @Description("Валидация полученного ответа String'ой")
+    public void validateResponse(TestCaseRunner runner,
+                                 String color,
+                                 String height,
+                                 String material,
+                                 String sound,
+                                 String wingsState
+    ) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body("{\n" +
+                        "  \"color\": \"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"material\": \"" + material + "\",\n" +
+                        "  \"sound\": \"" + sound + "\",\n" +
+                        "  \"wingsState\": \"" + wingsState + "\"\n" +
+                        "}")
+        );
+    }
+
+    @Description("Валидация полученного ответа из папки Resources")
+    public void validateResponse(TestCaseRunner runner, String expectedPayload) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body(new ClassPathResource(expectedPayload))
+        );
+    }
+
+    @Description("Валидация полученного ответа из папки Payload")
+    public void validateResponse(TestCaseRunner runner, Object expectedPayload) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body(new ObjectMappingPayloadBuilder(expectedPayload, new ObjectMapper()))
+        );
+    }
+
+    //endregion
+
 }
--- src/test/java/autotests/clients/DuckCrudClient.java
+++ src/test/java/autotests/clients/DuckCrudClient.java
@@ -3,8 +3,14 @@ package autotests.clients;
 import autotests.EndpointConfig;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.message.MessageType;
+import com.consol.citrus.message.builder.ObjectMappingPayloadBuilder;
 import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -59,4 +65,59 @@ public class DuckCrudClient extends TestNGCitrusSpringSupport {
                 .queryParam("wingsState", wingsState)
         );
     }
+
+    //region create - валидация
+
+    @Description("Валидация полученного ответа String'ой")
+    public void validateResponse(TestCaseRunner runner,
+                                 String color,
+                                 String height,
+                                 String id,
+                                 String material,
+                                 String sound,
+                                 String wingsState
+    ) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body("{\n" +
+                        "  \"color\": \"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"id\": " + id + ",\n" +
+                        "  \"material\": \"" + material + "\",\n" +
+                        "  \"sound\": \"" + sound + "\",\n" +
+                        "  \"wingsState\": \"" + wingsState + "\"\n" +
+                        "}")
+        );
+    }
+
+    @Description("Валидация полученного ответа из папки Resources")
+    public void validateResponse(TestCaseRunner runner, String expectedPayload) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body(new ClassPathResource(expectedPayload))
+        );
+    }
+
+    @Description("Валидация полученного ответа из папки Payload")
+    public void validateResponse(TestCaseRunner runner, Object expectedPayload) {
+        runner.$(http()
+                .client(duckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .type(MessageType.JSON)
+                .body(new ObjectMappingPayloadBuilder(expectedPayload, new ObjectMapper()))
+        );
+    }
+
+    //endregion
+
 }
