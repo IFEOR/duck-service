ad674d85b9d517137f9484e584d5d7c8869cb2be
A.Kitova <nazumy@bk.ru>
2023-07-28 18:43:48 +0600

    [+] @Step Allure annotations

--- src/test/java/autotests/clients/DuckCrudClient.java
+++ src/test/java/autotests/clients/DuckCrudClient.java
@@ -2,9 +2,11 @@ package autotests.clients;
 
 import autotests.BaseTest;
 import com.consol.citrus.TestCaseRunner;
+import io.qameta.allure.Step;
 
 public class DuckCrudClient extends BaseTest {
 
+    @Step("Эндпоинт создания уточки через json тело")
     public void create(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
         sendPostRequest(runner, "/api/duck/create", "{\n" +
                 "  \"color\": \"" + color + "\",\n" +
@@ -15,14 +17,17 @@ public class DuckCrudClient extends BaseTest {
                 "}");
     }
 
+    @Step("Эндпоинт создания уточки через payload")
     public void create(TestCaseRunner runner, Object payload) {
         sendPostRequest(runner, "/api/duck/create", payload);
     }
 
+    @Step("Эндпоинт получения списка id уточек")
     public void getAllIds(TestCaseRunner runner) {
         sendGetRequest(runner, "/api/duck/getAllIds");
     }
 
+    @Step("Эндпоинт изменения характеристик уточки с id = {id}")
     public void update(TestCaseRunner runner, String id, String color, String height, String material, String sound, String wingsState) {
         sendPutRequest(runner, "/api/duck/update?id=" + id +
                 "&color=" + color +
@@ -32,6 +37,7 @@ public class DuckCrudClient extends BaseTest {
                 "&wingsState=" + wingsState);
     }
 
+    @Step("Эндпоинт удаления уточки с id = {id}")
     public void delete(TestCaseRunner runner, String id) {
         sendDeleteRequest(runner, "/api/duck/delete", "id", id);
     }
